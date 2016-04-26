b73432761e36ac2d1b39fdc80c867808d2c6632b
JY.Wang <jy.wang@jy-wang.com>
2016-04-26 17:57:36 +0800

    2016年4月26日

--- index.php
+++ index.php
@@ -1,43 +1,51 @@
-<!DOCTYPE html>
+
 <html>
 <meta charset="UTF-8">
-<title>
-Search
-</title>
+<title>Search</title>
+
+<body>
 <form action="" method="post">
     <input type="text" name="key">
-    <input type="submit"  name="key" value="Search">
+    <input type="submit"  value="Search">
 </form>
 
 <?php
+
 include "config.php";
-$sql = "select * from qq WHERE username like '$_POST[key]%'";
 $conn = mysqli_connect($dbhost, $dbuser,$dbpass,$dbname);
-if(strlen($_POST['key'])>5)
+
+if(isset($_POST['key']))
 {
-    if (!$conn)
+    $sql = "select * from qq WHERE username like $_POST[key]%'";
+    if(strlen($_POST['key'])>4)
     {
-    die('Could not connect: ');
+        if (!$conn) {
+            die('Could not connect: ');
+        }
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0)
+        {
+            while ($row = $result->fetch_assoc())
+            {
+                echo "<br> QQ: " . $row["username"] . " ------- 密码" . $row["password"];
+            }
+        }
+        else
+        {
+            echo "0 results";
+        }
+        $conn->close();
     }
-    $result = $conn->query($sql);
- if ($result->num_rows>0)
-      {
-      while($row = $result->fetch_assoc())
-          {
-          echo "<br> QQ: ". $row["username"]." ------- 密码". $row["password"];
-          }
-      }
-  else
-      {
-      echo "0 results";
-      }
-  $conn->close();
+    else
+    {
+        echo "“".$_POST['key']."”小于5个字符";
+    }
 }
-else
-{
-  echo "错误";
-  echo $_POST['key'];
-}
+
+
 ?>
 
+
+</body>
 </html>
